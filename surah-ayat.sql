b476ca68550fd1625631195c0d33f634c241ded9
faheemfactor <faheemfactor@gmail.com>
2018-09-11 22:40:13 -0700

    Task - Ayah Report and Linking with Surah Report

--- surah-ayat.sql
+++ surah-ayat.sql
@@ -9,7 +9,17 @@ SELECT
     A.Ayah,
     A.Translation
 FROM QURAN_Ayah_List A
+WHERE   
+    A.Translation in ('Urdu')
 
+-- this query for parameter; parameter will display the surah names as user selects and click view report
+-- All ayath of surah will be displayed
+SELECT
+Id =  S.Surah_No,
+val = CONCAT( S.Surah_No, '-' ,  S.Surah_Revealed_Location_Arabic,'-' ,  S.Surah_Name_Arabic )
+FROM dbo.Surah S;
+
+    ---DataSet for Drop Down Parameter from Report
 
 /*
 
@@ -24,10 +34,14 @@ Things to do:
 
 Linking Surah report with this report 
 
+When you link a report to another report. you can specify parameter
+
 Hints :
 - Table 
 - Expressions on row background color
 - Aggregating of rows
+- Passing Parameter to Report
+- Link to Action
 
 
 */
